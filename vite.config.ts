f8a394573e908670cc3df4b20bd162b5168efaae
EJ <ejtechpro@gmail.com>
2024-10-31 11:39:35 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   clearScreen: true,
-  server: {
-    open: "/",
-  },
+  base: "/ej-portfolio/",
 });
